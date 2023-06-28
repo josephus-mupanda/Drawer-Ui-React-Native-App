f997a3e832a2e15d0bb241d0b2b845f88450d575
josephus-mupanda <josephusmupanda48@gmail.com>
2023-06-27 19:53:35 -0700

    commit this

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 const HomeScreen = () => {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home Screen</Text>
+            <Text>This Home Screen</Text>
         </View>
     );
 };
